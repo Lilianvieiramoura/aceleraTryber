927990771b7f0d068c7800b7d8ed9bcd2f9a0c83
Lílian Vieira <lilianv528@gmail.com>
2024-11-10 14:19:54 -0300

    Tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+import banner from '../assets/banner.svg'
+
 function Login() {
+
+const [input, setInput] = useState({
+  username: '',
+  password: '',
+});
+
+function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  // console.log(event.target.name, event.target.value);
+  
+  setInput({
+    ...input,
+    [event.target.name]: event.target.value
+  })
+}
+
+function handleSubmit(event: React.FormEvent) {
+  // console.log(input);
+  event.preventDefault();
+}
+
   return (
     <>
-      <h1>Login</h1>
+      <h1>Welcome</h1>
+      <img src={banner} alt="banner da tela de login" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" placeholder='Enter your usarname' id='username' name='username'/>
+        <input onChange={handleChange} type="password" placeholder='Enter your password' id='password' name='password'/>
+        <p>Forgout Passowrd?</p>
+        <button>Sign In</button>
+      </form>
     </>
   )
 }
